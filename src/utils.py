4aec123d12d814563230ce0839faaab7f5ec118d
Ekram <ekruiwc2020@gmail.com>
2023-11-28 10:33:54 +0300

    add dataset

--- src/utils.py
+++ src/utils.py
@@ -181,19 +181,19 @@ def convert_2_timestamp(column, data):
         return timestamp_
     else: print(f"{column} not in data")
 
-    def get_community_participation(path):
-        """ specify path to get json files"""
-        combined = []
-        comm_dict = {}
-        for json_file in glob.glob(f"{path}*.json"):
-            with open(json_file, 'r') as slack_data:
-                combined.append(slack_data)
-        # print(f"Total json files is {len(combined)}")
-        for i in combined:
-            a = json.load(open(i.name, 'r', encoding='utf-8'))
+def get_community_participation(path):
+    """ specify path to get json files"""
+    combined = []
+    comm_dict = {}
+    for json_file in glob.glob(f"{path}*.json"):
+        with open(json_file, 'r') as slack_data:
+            combined.append(slack_data)
+    # print(f"Total json files is {len(combined)}")
+    for i in combined:
+        a = json.load(open(i.name, 'r', encoding='utf-8'))
 
-            for msg in a:
-                if 'replies' in msg.keys():
-                    for i in msg['replies']:
-                        comm_dict[i['user']] = comm_dict.get(i['user'], 0)+1
-        return comm_dict
+        for msg in a:
+            if 'replies' in msg.keys():
+                for i in msg['replies']:
+                    comm_dict[i['user']] = comm_dict.get(i['user'], 0)+1
+    return comm_dict
