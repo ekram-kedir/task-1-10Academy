cf1a3630bb5d99cad7868b683c3253e9a7804757
Ekram <ekruiwc2020@gmail.com>
2023-12-02 01:47:42 +0300

    start designing the Dashboard

--- streamlit_dashboard/app.py
+++ streamlit_dashboard/app.py
@@ -34,28 +34,33 @@ with st.container():
     )
 
     # Distribution difference between consecutive messages in each channel
-    data_loader = SlackDataLoader(path='./../dataset/')
+    data_loader = SlackDataLoader(path='./dataset/')
     channels_data = data_loader.get_channels()
 
-    for channel in channels_data:
-        path_channel = f"../dataset/{channel['name']}"
-        time_differences = []
+    # for channel in channels_data:
+    path_channel = f"./dataset/all-week1/"
+    time_differences = []
 
-        stamps, _ = get_messages_reply_timestamp_from_channel(path_channel)
+    stamps, _ = get_messages_reply_timestamp_from_channel(path_channel)
 
-        # Calculate time differences between consecutive messages
-        for i in range(1, len(stamps)):
-            prev_msg_timestamp = float(stamps[i-1][0])
-            curr_msg_timestamp = float(stamps[i][0])
-            time_difference = curr_msg_timestamp - prev_msg_timestamp
-            time_differences.append(time_difference)
+    # Calculate time differences between consecutive messages
+    for i in range(1, len(stamps)):
+        prev_msg_timestamp = float(stamps[i-1][0])
+        curr_msg_timestamp = float(stamps[i][0])
+        time_difference = curr_msg_timestamp - prev_msg_timestamp
+        time_differences.append(time_difference)
 
-        # Plot histogram of time differences for the current channel
-        fig, ax = plt.subplots()
-        ax.hist(time_differences, bins=20, edgecolor='blue')
-        ax.set_xlabel('Time Difference (seconds)')
-        ax.set_ylabel('Frequency')
-        ax.set_title(f'Distribution of Time Differences Between Consecutive Messages\nChannel: {channel["name"]}')
+    # Plot histogram of time differences for the current channel
+    # Plot histogram of time differences for the current channel
+    fig, ax = plt.subplots()
+    ax.hist(time_differences, bins=20, edgecolor='blue')
+    ax.set_xlabel('Time Difference (seconds)')
+    ax.set_ylabel('Frequency')
+    ax.set_title(f'Distribution of Time Differences Between Consecutive Messages\nChannel: all-week1')
 
-        # Display the Matplotlib figure in Streamlit
-        st.pyplot(fig)
+    # Display the Matplotlib figure in Streamlit
+    st.pyplot(fig)
+
+    # Close the Matplotlib figure to avoid the warning
+    plt.close()
+
